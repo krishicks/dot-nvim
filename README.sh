da3568ad68f210161ef7b5fc0c79bb40877fd2d5
Kris Hicks <krishicks@gmail.com>
2017-07-25 18:34:55 -0700

    Update README for fork

--- README.sh
+++ README.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # step 1
-git clone https://github.com/vito/dot-nvim.git ~/.config/nvim --recursive
+git clone https://github.com/krishicks/dot-nvim.git ~/.config/nvim --recursive
 
 # there is no step 2
